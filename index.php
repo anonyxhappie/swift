b3bf6de222b11a9a9517946f7fcf455bc5ada1fc
anonyxhappie <akkilsl522@gmail.com>
2016-05-13 06:46:39 +0530

    Commit to whole project

---
+++ index.php
@@ -0,0 +1,139 @@
+<html>
+<head>
+   	 <link rel="shortcut icon" href="images/favicon.ico" />
+	   <title>We The Swift</title>
+	   <!--offline-->
+	   <script src="js/jquery.min.js"></script>
+	   <script src="js/bootstrap.min.js"></script>
+		 <link href="css/bootstrap.min.css" rel="stylesheet">
+	   <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+	   <!--online-->
+	   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+		 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+	   <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+	   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+      <style type="text/css">
+      	body {
+      		color:white;
+      		/*background-image: url("images/win.gif");*/
+      		background-color: black;/*#464646;*/
+          background-attachment: fixed;
+      	}
+
+      	div > h1.subtitle {
+      		width: 1224px;
+      		line-height: 36px;
+      		font-size: 24px;
+      		position: absolute;
+      		left: 32px;
+      		top: 254px;
+      	  text-align: center;
+  			  color: #fcfbfd;
+ 	    	  text-transform: uppercase;
+      	  letter-spacing: .10em;
+        }
+
+      	div > h2.title {
+      		width: 1224px;
+      		line-height: 72px;
+      		font-size: 72px;
+      		position: absolute;
+      		left: 52px;
+      		top: 308px;
+      		text-align: center;
+  			  color: #fcfbfd;
+  			  text-transform: uppercase;
+  			  font-family: 'National Thin';
+  			  letter-spacing: .16em;
+  			  font-weight: normal;
+
+      	}
+
+      	div > img.logo {
+      		width: 146px;
+      		height: 146px;
+      		position: absolute;
+      		left: 591px;
+      		top: 85px;
+      	}
+
+      	div > a.button1 {
+      		font-size: 20px;
+      		padding: 24px;
+      		position: absolute;
+      		left: 503px;
+      		top: 416px;
+      		background-color: transparent;
+  			border: 2px solid white;
+  			color: white;
+  			font-family: 'National Medium';
+  			letter-spacing: .16em;
+  			text-transform: uppercase;
+			text-decoration: none;
+      	}
+
+
+      	div > a.button1:hover {
+      		text-decoration: none;
+      		color: black;
+  			border: 2px solid white;
+  			background-color: white;
+      	}
+
+      	div > a.button2 {
+      		font-size: 20px;
+      		line-height: 28px;
+      		padding: 24px;
+      		position: absolute;
+      		left: 524px;
+      		top: 536px;
+      		background-color: white;
+  			border: 2px solid white;
+  			color: black;
+  			font-family: 'National Medium';
+  			letter-spacing: .16em;
+  			text-transform: uppercase;
+      	}
+
+      	div > a.button2:hover {
+      		background-color: transparent;
+  			border: 2px solid white;
+  			color: white;
+  			text-decoration: none;
+      	}
+
+        .bvideo {
+          width: 1349px;
+          height: 760px;
+          position: absolute;
+          top: -24px;
+          left: 0px;
+        }
+
+        .container:before {
+            background-color: black;
+            opacity: 0.2;
+        }
+      </style>
+</head>
+<body class="text-center">
+	<video autoplay="true" loop="true" class="bvideo">
+    <source type="video/mp4" src="images/back.mp4">
+    <source type="video/webm" src="images/back.webm">
+  </video>
+  <div class="container">
+  <div class="content">
+
+		<img class="logo" src="images/mickey.png">
+
+		<h1 class="subtitle">Websites We Design Ourselves</h1>
+		<h2 class="title">Join the Evolution</h2>
+
+		<a href="login.html" target="new" class="button1" ><span>Swift Messenger</span></a>
+		<a href="" target="new" class="button2" href="" >A Fashion Hub</a>
+
+</div>
+	</div>
+</body>
+</html>
